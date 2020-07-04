3cfe3014e41a0de835d1f385a40602862550d1e8
Henrique Pires <henrique.p.espindola@gmail.com>
2020-07-04 12:39:18 -0300

    Screen responsitivy. Other features

--- src/features/dashboard/Dashboard.js
+++ src/features/dashboard/Dashboard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 import { Button } from '@material-ui/core';
-import { Link } from 'react-router-dom';
+// import { Link } from 'react-router-dom';
 
 import ArrowDropUpIcon from '@material-ui/icons/ArrowDropUp';
 import Menu from '../../shared/components/menu/Menu';
 
-import DateButton from './components/dateButton/dateButton';
+// import DateButton from './components/dateButton/dateButton';
 import MotorContainer from './components/motorContainer/motorContainer';
 
 import './Dashboard.css';
@@ -22,20 +22,15 @@ const dashboard = () => {
   return (
     <>
       <Menu />
-      <div style={{ marginTop: 80 }} className="container">
-        <div style={{ marginBottom: 40 }} className="row">
+      <div style={{ marginTop: 20 }} className="container-fluid">
+        <div className="row">
           <div className="col-12">
-            <div className="col-md-6 textHeaderOperator">
-              Visualização do operador
-            </div>
-            <div className="col-md-6">
-              <DateButton />
-            </div>
+            <div className="col-sm-7 col-md-7 col-lg-7" />
           </div>
         </div>
         <div className="container-fluid componentAlign">
           <div className="row ">
-            <div className="btn col-1">
+            <div className="motors col-sm-3 col-md-3 col-lg-3">
               <Button>
                 <MotorContainer
                   motorName="Motor 1"
@@ -51,7 +46,7 @@ const dashboard = () => {
               </Button>
             </div>
 
-            <div className="btn col-1">
+            <div className="col-sm-3 col-md-3 col-lg-3">
               <Button>
                 <MotorContainer
                   motorName="Motor 2"
@@ -67,7 +62,7 @@ const dashboard = () => {
               </Button>
             </div>
 
-            <div className="btn col-1">
+            <div className="col-sm-3 col-md-3 col-lg-3">
               <Button>
                 <MotorContainer
                   motorName="Motor 3"
@@ -83,7 +78,7 @@ const dashboard = () => {
               </Button>
             </div>
 
-            <div className="btn col-1">
+            <div className="col-sm-3 col-md-3 col-lg-3">
               <Button>
                 <MotorContainer
                   motorName="Motor 4"
@@ -98,23 +93,23 @@ const dashboard = () => {
                 />
               </Button>
             </div>
-            <div className="container">
-              <div className="row">
-                <div className="btn col-sm-12 col-md-12 col-lg-12">
-                  <LineChart
-                    width={1200}
-                    height={500}
-                    data={data}
-                    margin={{ top: 20, right: 60, bottom: 5, left: -20 }}
-                  >
-                    <Line type="natural" dataKey="uv" stroke="#8884d8" />
-                    <CartesianGrid stroke="#ccc" strokeDasharray="3 3 " />
-                    <XAxis dataKey="name" />
-                    <YAxis />
-                  </LineChart>
-                </div>
-              </div>
-            </div>
+          </div>
+        </div>
+      </div>
+      <div className="graph container">
+        <div className="graph row">
+          <div className="graph col-sm-12 col-md-12 col-lg-12">
+            <LineChart
+              width={1290}
+              height={400}
+              data={data}
+              margin={{ top: 20, right: 150, bottom: 5, left: -30 }}
+            >
+              <Line type="natural" dataKey="uv" stroke="#8884d8" />
+              <CartesianGrid stroke="#ccc" strokeDasharray="3 3 " />
+              <XAxis dataKey="name" />
+              <YAxis />
+            </LineChart>
           </div>
         </div>
       </div>
---
+++ src/pages/motors/Motor.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import MotorContainer from '../../features/dashboard/components/motorContainer/motorContainer';
+
+const Motor = () => {
+  return <MotorContainer />;
+};
+
+export default Motor;
